c6697af27f7352c5cfb6f7fb82f2fd8ef6875af7
Akash Bhaskar <akash@revyrieglobal.com>
2023-09-28 01:29:29 +0530

    added navbar

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -18,7 +18,7 @@ export const HeroSection = () => {
               wrapper="span"
               speed={50}
               repeat={Infinity}
-            />{" "}
+            />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
@@ -30,7 +30,7 @@ export const HeroSection = () => {
             <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-black-500 to-green-700 hover:bg-slate-200 text-white">
               Hire me
             </button>
-            <button className="px-6 py-2 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-black-500 to-green-700 hover:bg-slate-200 text-white">
+            <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-black-500 to-green-700 hover:bg-slate-200 text-white mt-3">
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
                 Download CV
               </span>
