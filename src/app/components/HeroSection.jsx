c0e2abafba821b0cfaa3cfa6a679857e80099f86
Akash Bhaskar <akash@revyrieglobal.com>
2023-09-27 18:05:56 +0530

    header section

---
+++ src/app/components/HeroSection.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Image from "next/image";
+export const HeroSection = () => {
+  return (
+    <section>
+      <div className="grid grid-cols-1 lg:grid-cols-12">
+        <div className="col-span-7 place-self-center">
+          <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
+            Hello, I'm Akash Bhaskar
+          </h1>
+          <p className="text-[#ADB7BE] text-lg mb-6 lg:text-xl">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+          <div>
+            <button className="px-6 py-3 rounded-full mr-4">Hire me</button>
+            <button>Download CV</button>
+          </div>
+        </div>
+        <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+            <Image
+              src="/images/hero.png"
+              alt="hero image"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              width={300}
+              height={300}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
